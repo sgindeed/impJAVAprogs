700ece8358d4902669dac7f3050ec95dfe86fa1b
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2023-08-14 10:53:07 +0530

    Create acMod101.java

---
+++ acMod101.java
@@ -0,0 +1,11 @@
+// Java program to illustrate default modifier
+package p1;
+
+// Class Geek is having Default access modifier
+class Geek
+{
+	void display()
+	{
+		System.out.println("Hello World!");
+	}
+}
