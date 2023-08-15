501df3c41fd0d310da8a887997cbfb19ccd25b14
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2023-08-15 23:25:59 +0530

    Update 2Dbasic.java

--- Assignment/2Dbasic.java
+++ Assignment/2Dbasic.java
@@ -1,1 +1,19 @@
+public class TwoDArrayExample {
+    public static void main(String[] args) {
+        // Define a 2D array with different numbers of columns in different rows
+        int[][] twoDArray = {
+            {1, 2, 3},
+            {4, 5},
+            {6, 7, 8, 9}
+        };
+
+        // Access and print elements of the 2D array
+        for (int i = 0; i < twoDArray.length; i++) {
+            for (int j = 0; j < twoDArray[i].length; j++) {
+                System.out.print(twoDArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
 
