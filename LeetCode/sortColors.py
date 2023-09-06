f41f921538a36243fecf61503889b395c345dde1
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2023-09-06 15:32:47 +0530

    Create sortColors.py

---
+++ LeetCode/sortColors.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        n = len(nums)
+        for i in range(n):
+            swapped = False
+            for j in range(0, n-i-1):
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+                    swapped = True
+            if not swapped:
+                break
